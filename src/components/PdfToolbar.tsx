b83e0992da8a469f22450bb541a5754687be59a7
Sage <tuzzy08@gmail.com>
2024-01-04 12:50:53 +0100

    Added document navigation

--- src/components/PdfToolbar.tsx
+++ src/components/PdfToolbar.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 
-export function PdfToolbar() {
+export function PdfToolbar({
+	numberOfPages,
+	currentPage,
+	setCurrentPage,
+}: {
+	numberOfPages: number | undefined;
+	currentPage: number;
+	setCurrentPage: Dispatch<SetStateAction<number>>;
+}) {
 	return (
 		<div className='flex justify-center items-center border-b gap-1'>
 			<div className='flex items-center gap-2'>
-				<Input className='w-10 h-7' />
-				<span className='text-zinc-700 text-sm'>350</span>
+				<Input className='w-10 h-7' value={currentPage} />
+				<span className='text-zinc-700 text-sm'>{numberOfPages ?? null}</span>
 			</div>
 
 			<div className='flex items-center'>
-				<Button className='p-1' variant='ghost' aria-label='Next Page Button'>
+				<Button
+					className='p-1'
+					variant='ghost'
+					aria-label='Next Page Button'
+					onClick={() => {
+						if (currentPage === numberOfPages) return;
+						setCurrentPage((prev) => prev + 1);
+					}}
+				>
 					<ChevronDown className='h-7 w-7' />
 				</Button>
 				<Button
 					className='p-1'
 					variant='ghost'
 					aria-label='Previous Page Button'
+					onClick={() => {
+						if (currentPage === 1) return;
+						setCurrentPage((prev) => prev - 1);
+					}}
 				>
 					<ChevronUp className='h-7 w-7' />
 				</Button>
--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -8,13 +9,23 @@ import { useResizeDetector } from 'react-resize-detector';
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 export function PdfViewer({ url }: { url: string }) {
+	const [totalPages, setTotalPages] = useState<number>();
+	const [currentPage, setCurrentPage] = useState<number>(1);
 	const { width, height, ref } = useResizeDetector();
+
+	const onLoadSuccess = ({ numPages }: { numPages: number }) => {
+		setTotalPages(numPages);
+	};
 	return (
 		<div className='flex flex-col bg-white border'>
-			<PdfToolbar />
+			<PdfToolbar
+				numberOfPages={totalPages}
+				setCurrentPage={setCurrentPage}
+				currentPage={currentPage}
+			/>
 			<div ref={ref}>
-				<Document file={url}>
-					<Page pageNumber={1} width={width}></Page>
+				<Document file={url} onLoadSuccess={onLoadSuccess}>
+					<Page pageNumber={currentPage} width={width} />
 				</Document>
 			</div>
 		</div>
